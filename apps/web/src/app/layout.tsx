74ac0fff336df2e88b953b731b4b74de1c72db43
morg-ma <mahmoud.ahmed.25.9.24@gmail.com>
2026-05-19 19:06:07 +0300

    remove init dashboard and fix the format

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -27,12 +27,15 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning className="dark">
       <head>
-        <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
+          rel="stylesheet"
+        />
       </head>
-      <body className={`${geistSans.variable} ${jetbrainsMono.variable} antialiased`}>
-        <Providers>
-          {children}
-        </Providers>
+      <body
+        className={`${geistSans.variable} ${jetbrainsMono.variable} antialiased`}
+      >
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
