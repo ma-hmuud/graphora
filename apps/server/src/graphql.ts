bd1007f5d45ff377225d555b5a4d5cdd040fccda
morg-ma <mahmoud.ahmed.25.9.24@gmail.com>
2026-05-20 21:03:04 +0300

    refactor: Update User and Session models to use Int for IDs and adjust related fields

--- apps/server/src/graphql.ts
+++ apps/server/src/graphql.ts
@@ -1,3 +1,4 @@
+
 /*
  * -------------------------------------------------------
  * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
@@ -8,28 +9,25 @@
 /* eslint-disable */
 
 export enum Plan {
-  FREE = "FREE",
-  PRO = "PRO",
+    FREE = "FREE",
+    PRO = "PRO"
 }
 
-export class User {
-  id!: number;
-  name!: string;
-  email!: string;
-  emailVerified!: boolean;
-  image?: Nullable<string>;
-  passwordHash?: Nullable<string>;
-  plan!: Plan;
-  createdAt!: string;
-  updatedAt!: string;
+export interface User {
+    id: number;
+    name: string;
+    email: string;
+    emailVerified: boolean;
+    image?: Nullable<string>;
+    passwordHash?: Nullable<string>;
+    plan: Plan;
+    createdAt: string;
+    updatedAt: string;
 }
 
-export abstract class IQuery {
-  abstract hello(
-    name?: Nullable<string>,
-  ): Nullable<string> | Promise<Nullable<string>>;
-
-  abstract me(): Nullable<User> | Promise<Nullable<User>>;
+export interface IQuery {
+    hello(name?: Nullable<string>): Nullable<string> | Promise<Nullable<string>>;
+    me(): Nullable<User> | Promise<Nullable<User>>;
 }
 
 type Nullable<T> = T | null;
